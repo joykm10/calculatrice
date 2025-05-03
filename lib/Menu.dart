f17c3bba425798b5c7d53f955702c6c287a5eec3
joykm10 <24km169si@udbl.ac.cd>
2025-05-03 14:56:28 +0200

    Ajout des menus et des operations

---
+++ lib/Menu.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void menu(){
+  print("1. Addition");
+  print("2. Soustraire");
+  print("3. Multiplication");
+  print("4. Diviser");
+  print("5. Quitter");
+
+}
+String? choix(){
+  print("Choisissez une operation :");
+  return stdin.readLineSync();
+}
+bool userchoix(String choix){
+return [ "1", "2", "3", "4", "5"].contains(choix);
+}
---
+++ lib/operation.dart
@@ -0,0 +1,13 @@
+
+double addition(double a, double b) => a + b;
+double soustraction(double a, double b) => a - b;
+
+
+
+
+double multiplication(double a, double b){
+  return a * b;
+}
+double division(double a, double b){
+  return a / b;
+}
